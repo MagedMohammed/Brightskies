ea6f6b81324ef7f9cfd612279f6b72a6f7ea85d5
Maged Mohammed <maged.mo.it@gmail.com>
2021-06-13 01:30:53 +0200

    update DetailOfARecipeViewController

--- BrightskiesTest/DetailOfARecipe/View/DetailOfARecipeViewController.swift
+++ BrightskiesTest/DetailOfARecipe/View/DetailOfARecipeViewController.swift
@@ -11,15 +11,13 @@ class DetailOfARecipeViewController: UIViewController {
 
     //    MARK:- Outlet
     @IBOutlet weak var recipeImage: UIImageView!
-    
     @IBOutlet weak var recipeTitleLabel: UILabel!
     @IBOutlet weak var recipeRateLabel: UILabel!
     @IBOutlet weak var recipeTimeLabel: UILabel!
-    
     @IBOutlet weak var recipeIngredientsLabel: UILabel!
-    
     @IBOutlet weak var recipeDifficultyLabel: UILabel!
-    
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var ingredientsTableView: UITableView!
     
     
     //    MARK:- Properties
@@ -27,9 +25,12 @@ class DetailOfARecipeViewController: UIViewController {
     //    MARK:- ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.ingredientsTableView.delegate = self
+        self.ingredientsTableView.dataSource = self
         self.setColorOfNav()
         self.setview(data: self.recipeDetails)
     }
+    
     //    MARK:- Method
     func setview(data:RecipesResponseModelElement){
         let imageURL = URL(string:data.image ?? "")
@@ -38,7 +39,20 @@ class DetailOfARecipeViewController: UIViewController {
         self.recipeTimeLabel.text = data.time ?? ""
         self.recipeRateLabel.text = String(data.ratings ?? 0)
         self.recipeIngredientsLabel.text = "\(data.ingredients?.count ?? 0) Ingredients"
+        self.descriptionLabel.text = data.recipesResponseModelDescription ?? ""
     }
-    //    MARK:- Action
 
+}
+extension DetailOfARecipeViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return recipeDetails.ingredients?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let data = recipeDetails.ingredients ?? [String]()
+        cell.selectionStyle = .none
+        cell.textLabel?.text = "* \(data[indexPath.row])"
+        return cell
+    }
 }
