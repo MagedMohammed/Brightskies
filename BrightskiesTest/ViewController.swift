3ce11c139f286a2854f345cc8e79078e7875b6d9
Maged Mohammed <maged.mo.it@gmail.com>
2021-06-12 16:37:52 +0200

    Initial Commit

---
+++ BrightskiesTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BrightskiesTest
+//
+//  Created by Maged Omar on 12/06/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
