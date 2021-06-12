aa20ba7b045f4e7b94fc051a7d2879e3a2456e0c
Maged Mohammed <maged.mo.it@gmail.com>
2021-06-12 20:23:25 +0200

    create Logic "RecipesTableViewCell"

--- BrightskiesTest/ListOfRecipes/View/Cell/RecipesTableViewCell.swift
+++ BrightskiesTest/ListOfRecipes/View/Cell/RecipesTableViewCell.swift
@@ -6,13 +6,24 @@
 //
 
 import UIKit
+import PINRemoteImage
 
 class RecipesTableViewCell: UITableViewCell {
 
+    @IBOutlet weak var recipesImage: UIImageView!
+
+    @IBOutlet weak var recipesTitleLabel: UILabel!
+    
+    @IBOutlet weak var recipesTimeLabel: UILabel!
+    
+    @IBOutlet weak var recipeCalLabel: UILabel!
+    
+    @IBOutlet weak var recipesRateLabel: UILabel!
+    
+    @IBOutlet weak var recipesFavLabel: UILabel!
     
     
-    
-    
+//    MARK:- ViewLifeCycle
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -23,5 +34,23 @@ class RecipesTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
+    func setCell(data:RecipesResponseModelElement){
+        let imageURL = URL(string:data.image ?? "")
+        self.recipesImage.pin_setImage(from: imageURL)
+        self.recipesTitleLabel.text = data.name
+        self.recipeCalLabel.text = "\(data.calories ?? "") Cal"
+        self.recipesTimeLabel.text = data.time ?? ""
+        self.recipesRateLabel.text = String(data.ratings ?? 0)
+        self.recipesFavLabel.text = String(data.favorites ?? 0)
+    }
+    
+    
+    
+//    MARK:- Action
+    
+    @IBAction func shareButtonTapped(_ sender: UIButton) {
+        
+    }
+    
     
 }
