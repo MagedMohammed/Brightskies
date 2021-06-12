82b596293211f39c80a9940ad7604cc8571dc997
Maged Mohammed <maged.mo.it@gmail.com>
2021-06-12 20:06:40 +0200

    Create RecipesTableViewCell

---
+++ BrightskiesTest/ListOfRecipes/View/Cell/RecipesTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RecipesTableViewCell.swift
+//  BrightskiesTest
+//
+//  Created by Maged Omar on 12/06/2021.
+//
+
+import UIKit
+
+class RecipesTableViewCell: UITableViewCell {
+
+    
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
